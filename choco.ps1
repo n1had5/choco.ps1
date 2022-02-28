976f2cbc72d074efad411d3af8d6d96b885e1d51
Mohammad Nihad <n1had5@protonmail.com>
2022-03-01 01:26:51 +0200

    Added confirmation bypass

--- choco.ps1
+++ choco.ps1
@@ -3,9 +3,14 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 # install chocolatey if not installed
+
 if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
   Invoke-Expression((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
+
+# prompt for confirmation in scripts or bypass
+
+choco feature enable -n=allowGlobalConfirmation
 
 # for each package in the list run install
 Get-Content ".\packages" | ForEach-Object{($_ -split "\r\n")[0]} | ForEach-Object{choco install -y $_}
